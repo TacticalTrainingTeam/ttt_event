c5439aa1279b3f31236d0d5af99103a096241d3c
Andx <andx667@protonmail.com>
2025-11-28 16:54:20 +0100

    fix path

--- addons/no_chain/script_component.hpp
+++ addons/no_chain/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT no_chain
 #define COMPONENT_BEAUTIFIED No Chain
 
-#include "\z\ttt\addons\main\script_mod.hpp"
-#include "\z\ttt\addons\main\script_macros.hpp"
+#include "\z\ttt_event\addons\main\script_mod.hpp"
+#include "\z\ttt_event\addons\main\script_macros.hpp"
